5e8cf7ac5e669d6ca9e6fa0721ff42748758be38
Joel <joel@zeinestone.com>
2023-09-14 11:04:40 -0500

    disabled the oauth connection for now

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from src.youtube_utils.youtube_oauth import authenticate
 initialize_db()  # Initialize the database
 print('Database Initialized')
 
-config.youtube_client = authenticate()  # Modify the global youtube_client from config.py
-print('Youtube Client initialized')
-print(config.youtube_client)
+#config.youtube_client = authenticate()  # Modify the global youtube_client from config.py
+#print('Youtube Client initialized')
+#print(config.youtube_client)
 
 config.bot.run(config.DISCORD_TOKEN)
