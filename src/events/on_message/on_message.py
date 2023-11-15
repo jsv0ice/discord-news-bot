a4b00ed2210a9aea0f72e478bc0795e959f5e7c7
Joel <joel@zeinestone.com>
2023-11-14 21:42:29 -0600

    adding some logic to add messages to previous threads

--- src/events/on_message/on_message.py
+++ src/events/on_message/on_message.py
@@ -1,5 +1,6 @@
 import sqlite3
 import json
+from datetime import datetime
 from ...database import *
 from ...discord_utils import *
 from ...openai_utils.generate_thread_name.generate_thread_name import *
@@ -11,28 +12,42 @@ channels = fetch_channels()
 @bot.event
 async def on_message(message):
     if str(message.guild.id) == str(GUILD_ID):
-        
         with sqlite3.connect(DB_STRING) as conn:
             cursor = conn.cursor()
             cursor.execute("SELECT channel FROM subscriptions WHERE sub=?", (message.channel.id,))
             subscribed_channels = cursor.fetchall()
-            
-            # Create a new entry in the message_tracking table for the message
+
+            # Check if the current message and the previous message are within one minute
+            try:
+                last_messages = await message.channel.history(limit=2).flatten()
+                if len(last_messages) > 1:
+                    last_message = last_messages[1]
+                    time_difference = (message.created_at - last_message.created_at).total_seconds()
+                    if time_difference < 60:
+                        # If within one minute, check if the last message has a thread
+                        if last_message.thread:
+                            # Post current message in the previous message's thread
+                            await last_message.thread.send(content=message.content, embeds=message.embeds, files=[await attachment.to_file() for attachment in message.attachments])
+                            return
+            except Exception as e:
+                print(f"Error in checking message timestamps: {e}")
+
+            # Rest of your original code for normal message processing starts here
             message_id = str(message.id)
             content = message.content
             sent_status = 0  # 0 for not sent, 1 for sent successfully, -1 for error
             target_channels = json.dumps([])  # Initialize with an empty list
             
             cursor.execute("INSERT INTO message_tracking (message_id, content, sent_status, target_channels) VALUES (?, ?, ?, ?)",
-                            (message_id, content, sent_status, target_channels))
+                           (message_id, content, sent_status, target_channels))
             conn.commit()
 
             # Generate a name for the thread using OpenAI
             thread_name = await generate_thread_name(message.content, message.embeds, message.attachments)
             if thread_name[0] == '"':
                 thread_name = thread_name[1:]
-            if thread_name[len(thread_name) - 1] == '"':
-                thread_name = thread_name[:(len(thread_name) - 2)]
+            if thread_name[-1] == '"':
+                thread_name = thread_name[:-1]
                 
             for (channel_id,) in subscribed_channels:
                 target_channel = bot.get_channel(int(channel_id))
