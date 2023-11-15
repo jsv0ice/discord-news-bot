68e66d205d8dc7e4848e4f08d0befa71d9bdeabb
Joel <joel@zeinestone.com>
2023-11-14 22:32:49 -0600

    migrated to use the new openai models

--- src/openai_utils/generate_thread_name/generate_thread_name.py
+++ src/openai_utils/generate_thread_name/generate_thread_name.py
@@ -1,7 +1,7 @@
 import openai
 from ...config import OPENAI_TOKEN
 
-openai.api_key = OPENAI_TOKEN
+client = openai.AsyncOpenAI(api_key=OPENAI_TOKEN)
 
 async def generate_thread_name(content, embeds, files):
     print(content, embeds, files)
@@ -18,12 +18,14 @@ async def generate_thread_name(content, embeds, files):
     else:
         content_text = ''
     combined_text = f"{content_text} {embed_text} {file_text}"
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+    response = await client.chat.completions.create(
+        model="gpt-4-vision-preview",
         messages=[
             {"role": "system", "content": "You are a forum power user, and you are creating the perfect post title to summarize the content you receive."},
             {"role": "system", "content": "You will respond only with the perfect title, and nothing else"},
             {"role": "user", "content": combined_text[0:2000]},
         ]
     )
-    return(response['choices'][0]['message']['content'])
+    print(response)
+    return response.choices[0].message.content
+
